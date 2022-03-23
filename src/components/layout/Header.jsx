2d6f6216d890c0188e04bad04608e87d00a379d0
Eniola Ajayi <ajayieniola16@gmail.com>
2022-03-23 05:10:17 +0100

    setup router

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,9 +1,10 @@
+import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header>
       <div className="logo">DP Generator</div>
       <nav>
-        <a href="/">How it works</a>
+        <Link to="/how">How it works</Link>
       </nav>
     </header>
   );
