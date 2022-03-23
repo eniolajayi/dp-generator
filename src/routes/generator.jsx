471927240a5b9d0ea1e528aef0efbb62522681bc
Eniola Ajayi <ajayieniola16@gmail.com>
2022-03-23 15:02:13 +0100

    complete core logic

--- src/routes/generator.jsx
+++ src/routes/generator.jsx
@@ -1,7 +1,12 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import ReactCrop from "react-image-crop";
+import "react-image-crop/dist/ReactCrop.css";
 
 export default function DPGenerator() {
   const [imgSrc, setImgSrc] = useState("");
+  const [crop, setCrop] = useState();
+  const [completedCrop, setCompletedCrop] = useState();
+  const imgRef = useRef(null);
   const onSelectFile = (e) => {
     if (e.target.files && e.target.files.length > 0) {
       const reader = new FileReader();
@@ -12,12 +17,29 @@ export default function DPGenerator() {
     }
   };
 
+  const onImageLoad = (e) => {
+    imgRef.current = e.currentTarget;
+    const { width, height } = e.currentTarget;
+  };
+
   return (
     <>
       <h1>Generate DP</h1>
-      {/* User uploads image */}
       <input type="file" accept="image/*" onChange={onSelectFile} />
-      {Boolean(imgSrc) && <img src={imgSrc} alt="Select an area" />}
+      {Boolean(imgSrc) && (
+        <ReactCrop
+          crop={crop}
+          onChange={(_, percentCrop) => {
+            setCrop(percentCrop);
+          }}
+          onComplete={(_, percentCrop) => {
+            setCompletedCrop(percentCrop);
+            console.log(percentCrop);
+          }}
+        >
+          <img src={imgSrc} alt="Select an area" onLoad={onImageLoad} />
+        </ReactCrop>
+      )}
       {/* Allow then select a part */}
       {/* create data structure from selected part */}
     </>
