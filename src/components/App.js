869fe4b71fa943e3a3060a1406b0321b7448acd2
Eniola Ajayi <ajayieniola16@gmail.com>
2022-03-23 04:54:03 +0100

    create basic components and simple page

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import Button from "./Button/Button";
+import Layout from "./layout/Layout";
 
 function App() {
-  return <div></div>;
+  return (
+    <Layout>
+      <main>
+        <h1
+          style={{
+            fontSize: "2rem",
+            fontWeight: "600",
+            width: "min(30ch,100%)",
+            textAlign: "center",
+            textTransform: "uppercase",
+          }}
+        >
+          Create your own personalized dp banner with ease
+        </h1>
+        <Button>Get Started</Button>
+      </main>
+    </Layout>
+  );
 }
 
 export default App;
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="logo">DP Generator</div>
+      <nav>
+        <a href="/">How it works</a>
+      </nav>
+    </header>
+  );
+}
