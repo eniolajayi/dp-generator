8d22af1fb6aa3a5377b9dd2921210d8cf5861db0
Eniola Ajayi <ajayieniola16@gmail.com>
2022-03-26 10:20:00 +0100

    fix: https issue

--- src/utils/api.js
+++ src/utils/api.js
@@ -18,7 +18,7 @@ export async function getBannerInfo(bannerid) {
   try {
     const response = await axios({
       method: "get",
-      url: `http://dp-generator-api.herokuapp.com/${bannerid}/`,
+      url: `https://dp-generator-api.herokuapp.com/${bannerid}/`,
     });
     return response;
   } catch (error) {
@@ -30,7 +30,7 @@ export async function makeBanner(data, id) {
   try {
     const response = await axios({
       method: "post",
-      url: `http://dp-generator-api.herokuapp.com/make/dp/${id}/`,
+      url: `https://dp-generator-api.herokuapp.com/make/dp/${id}/`,
       data: data,
       headers: { "Content-Type": "multipart/form-data" },
     });
