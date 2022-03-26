5f80899219b33c4d300fe9c2f5193f481117fce3
Eniola Ajayi <ajayieniola16@gmail.com>
2022-03-26 03:58:38 +0100

    create basic layout of generatedp page

--- src/routes/generatedp.jsx
+++ src/routes/generatedp.jsx
@@ -1,35 +1,61 @@
 import { useParams, Link } from "react-router-dom";
-import { createStyles, Text, Button, Container } from "@mantine/core";
+import {
+  createStyles,
+  Text,
+  Button,
+  Container,
+  Group,
+  Title,
+} from "@mantine/core";
 import { getBannerInfo } from "../utils/api";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { PlusSquare } from "../components/icons";
 
 const useStyles = createStyles((theme, _params, getRef) => ({}));
 export default function GenerateDP() {
   const [data, setData] = useState({});
   let { bannerid } = useParams();
 
-  getBannerInfo(bannerid)
-    .then((res) => {
-      console.table(res.data);
-      setData(res.data);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+  useEffect(() => {
+    getBannerInfo(bannerid)
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, [bannerid]);
 
   const { classes } = useStyles();
   return (
-    <Container>
-      <Text order="1"> Your banner was created successfully!</Text>
-      <Text>{bannerid}</Text>
+    <Container sx={{ marginBottom: "2rem" }}>
+      <Title order={1}>{data.Name}</Title>
+      <Title order={3}>Description</Title>
+      <Text size="lg" lineClamp={4}>
+        {data.Description}
+      </Text>
       <img src={data.Banner} alt="banner" />
-      <Text>{data.Name}</Text>
-      <Text>{data.Description}</Text>
-      <div>
+      <Group>
+        <Button
+          variant="subtle"
+          color="indigo"
+          size="sm"
+          leftIcon={<PlusSquare />}
+        >
+          Upload Image
+        </Button>
+        <Text color="indigo">
+          your image will replace the highlighted portion of the banner
+        </Text>
+      </Group>
+      <Group>
+        <Button color="teal" size="md">
+          Generate Banner
+        </Button>
         <Button color="indigo" size="md" component={Link} to="/createdp">
           Create New Banner
         </Button>
-      </div>
+      </Group>
     </Container>
   );
 }
