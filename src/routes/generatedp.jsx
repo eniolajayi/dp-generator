16d29cbaac5159e0c85a339ee8f4b6479bd9395e
Eniola Ajayi <ajayieniola16@gmail.com>
2022-04-01 20:40:27 +0100

    remove redundant box element

--- src/routes/generatedp.jsx
+++ src/routes/generatedp.jsx
@@ -112,161 +112,159 @@ export default function GenerateDP() {
 
   return (
     <Container sx={{ marginBottom: "2rem" }}>
-      <Box>
+      <Skeleton visible={loading}>
+        <div className={classes.header}>
+          <Title order={1}>{data.Name}</Title>
+          <Spoiler maxHeight={200} showLabel="Read more" hideLabel="Hide">
+            <Text size="lg" sx={{ width: "min(45ch, 100%)" }}>
+              {data.Description}
+            </Text>
+          </Spoiler>
+        </div>
+      </Skeleton>
+      <section>
+        <Text color="indigo" size="sm" sx={{ marginBottom: "0.87rem" }}>
+          Create your personalized dp banner
+        </Text>
+        {/*TODO solve for round crop */}
         <Skeleton visible={loading}>
-          <div className={classes.header}>
-            <Title order={1}>{data.Name}</Title>
-            <Spoiler maxHeight={200} showLabel="Read more" hideLabel="Hide">
-              <Text size="lg" sx={{ width: "min(45ch, 100%)" }}>
-                {data.Description}
-              </Text>
-            </Spoiler>
-          </div>
+          <ReactCrop crop={area} disabled={true} onChange={(c) => setArea(c)}>
+            {Boolean(!imgUrl) ? (
+              <Image
+                src={data.Banner}
+                alt="banner"
+                withPlaceholder
+                sx={(theme) => ({
+                  position: "relative",
+                  "&::before": {
+                    content: '""',
+                    position: "absolute",
+                    top: `${data.Position_y}px`,
+                    left: `${data.Position_x}px`,
+                    background: theme.black,
+                    width: `${data.Width}px`,
+                    height: `${data.Height}px`,
+                    opacity: "0.4",
+                    zIndex: "3",
+                  },
+                })}
+              />
+            ) : (
+              <Image src={imgUrl} alt="new banner" />
+            )}
+          </ReactCrop>
         </Skeleton>
-        <section>
-          <Text color="indigo" size="sm" sx={{ marginBottom: "0.87rem" }}>
-            Create your personalized dp banner
-          </Text>
-          {/*TODO solve for round crop */}
-          <Skeleton visible={loading}>
-            <ReactCrop crop={area} disabled={true} onChange={(c) => setArea(c)}>
-              {Boolean(!imgUrl) ? (
-                <Image
-                  src={data.Banner}
-                  alt="banner"
-                  withPlaceholder
-                  sx={(theme) => ({
-                    position: "relative",
-                    "&::before": {
-                      content: '""',
-                      position: "absolute",
-                      top: `${data.Position_y}px`,
-                      left: `${data.Position_x}px`,
-                      background: theme.black,
-                      width: `${data.Width}px`,
-                      height: `${data.Height}px`,
-                      opacity: "0.4",
-                      zIndex: "3",
-                    },
-                  })}
-                />
-              ) : (
-                <Image src={imgUrl} alt="new banner" />
-              )}
-            </ReactCrop>
-          </Skeleton>
-        </section>
-        <Dropzone
-          multiple={false}
-          maxSize={MAX_FILE_SIZE}
-          accept={[MIME_TYPES.png, MIME_TYPES.jpeg]}
-          classNames={{
-            root: classes.dropzoneRoot,
-          }}
-          onDrop={(files) => {
-            notifications.showNotification({
-              color: "teal",
-              title: "File accepted",
-              message: "File Upload successful!",
-            });
-            onFileDrop(files);
-          }}
-          onReject={(files) => {
-            notifications.showNotification({
-              color: "red",
-              title: "Error",
-              message: "file did not meet restrictions, Try again.",
-            });
+      </section>
+      <Dropzone
+        multiple={false}
+        maxSize={MAX_FILE_SIZE}
+        accept={[MIME_TYPES.png, MIME_TYPES.jpeg]}
+        classNames={{
+          root: classes.dropzoneRoot,
+        }}
+        onDrop={(files) => {
+          notifications.showNotification({
+            color: "teal",
+            title: "File accepted",
+            message: "File Upload successful!",
+          });
+          onFileDrop(files);
+        }}
+        onReject={(files) => {
+          notifications.showNotification({
+            color: "red",
+            title: "Error",
+            message: "file did not meet restrictions, Try again.",
+          });
+        }}
+      >
+        {(status, theme) => dropzoneChildren(status, theme, data)}
+      </Dropzone>
+      {TEXT_IS_AVAILABLE && (
+        <Group>
+          <form
+            className={classes.form}
+            onSubmit={form.onSubmit((values) => {
+              handleSubmit(values);
+            })}
+          >
+            <TextInput
+              label="Enter your name"
+              variant="filled"
+              size="md"
+              maxLength={200}
+              className={classes.input}
+              value={form.values.name}
+              {...form.getInputProps("name")}
+              required
+            />
+            <TextInput
+              label="Enter full name of university"
+              variant="filled"
+              size="md"
+              maxLength={300}
+              className={classes.input}
+              value={form.values.university}
+              {...form.getInputProps("university")}
+              required
+            />
+            <Group position="left" mt="md">
+              <Button
+                disabled={!isReady()}
+                type="submit"
+                size="md"
+                color="blue"
+              >
+                Generate Banner
+              </Button>
+            </Group>
+          </form>
+        </Group>
+      )}
+      <Paper sx={{ margin: "2rem 0" }} shadow={"xs"} p="xs">
+        <Text color="dark">
+          Copy and Share this url to others, so they can create a customized dp
+          using the banner above
+        </Text>
+        <Text
+          color={"dark"}
+          weight={600}
+          size="md"
+          sx={{
+            overflowX: "scroll",
+            maxWidth: "min(800px, 100%)",
+            height: "80px",
+            padding: "1rem 0.875rem",
           }}
         >
-          {(status, theme) => dropzoneChildren(status, theme, data)}
-        </Dropzone>
-        {TEXT_IS_AVAILABLE && (
-          <Group>
-            <form
-              className={classes.form}
-              onSubmit={form.onSubmit((values) => {
-                handleSubmit(values);
-              })}
-            >
-              <TextInput
-                label="Enter your name"
-                variant="filled"
-                size="md"
-                maxLength={200}
-                className={classes.input}
-                value={form.values.name}
-                {...form.getInputProps("name")}
-                required
-              />
-              <TextInput
-                label="Enter full name of university"
-                variant="filled"
-                size="md"
-                maxLength={300}
-                className={classes.input}
-                value={form.values.university}
-                {...form.getInputProps("university")}
-                required
-              />
-              <Group position="left" mt="md">
-                <Button
-                  disabled={!isReady()}
-                  type="submit"
-                  size="md"
-                  color="blue"
-                >
-                  Generate Banner
-                </Button>
-              </Group>
-            </form>
-          </Group>
-        )}
-        <Paper sx={{ margin: "2rem 0" }} shadow={"xs"} p="xs">
-          <Text color="dark">
-            Copy and Share this url to others, so they can create a customized
-            dp using the banner above
-          </Text>
-          <Text
-            color={"dark"}
-            weight={600}
-            size="md"
-            sx={{
-              overflowX: "scroll",
-              maxWidth: "min(800px, 100%)",
-              height: "80px",
-              padding: "1rem 0.875rem",
-            }}
-          >
-            <span>{shareUrl}</span>
-          </Text>
-        </Paper>
-        <Group>
-          {Boolean(imgUrl) && (
-            <Button
-              disabled={Boolean(!imgUrl)}
-              href={imgUrl}
-              download="my_banner"
-              target="_blank"
-              color="teal"
-              size="md"
-              component="a"
-              role={"button"}
-            >
-              Download Banner
-            </Button>
-          )}
+          <span>{shareUrl}</span>
+        </Text>
+      </Paper>
+      <Group>
+        {Boolean(imgUrl) && (
           <Button
-            color="indigo"
-            variant="outline"
+            disabled={Boolean(!imgUrl)}
+            href={imgUrl}
+            download="my_banner"
+            target="_blank"
+            color="teal"
             size="md"
-            component={Link}
-            to="/createdp"
+            component="a"
+            role={"button"}
           >
-            Create New Banner
+            Download Banner
           </Button>
-        </Group>
-      </Box>
+        )}
+        <Button
+          color="indigo"
+          variant="outline"
+          size="md"
+          component={Link}
+          to="/createdp"
+        >
+          Create New Banner
+        </Button>
+      </Group>
     </Container>
   );
 }
