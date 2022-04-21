d1ef89c4cce67d2aa2dbb80892d79ce16d3c7cf9
Eniola Ajayi <ajayieniola16@gmail.com>
2022-04-21 10:54:13 +0100

    add comment

--- src/routes/banner.jsx
+++ src/routes/banner.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 
 export default function Banner() {
+  /* Will contain dashboard, header, settings 
+    or any function relating to banners */
   return (
     <>
       <Outlet />
