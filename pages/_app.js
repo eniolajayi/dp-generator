bd014fd99e80577ac43d1fef28ac37163dd25b4e
eniolajayi <ajayieniola16@gmail.com>
2023-03-26 05:06:27 +0100

    setup mantine style library

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,32 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Head from 'next/head';
+import { MantineProvider } from "@mantine/core";
+import { Notifications } from '@mantine/notifications';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App(props) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Auto DP Generator</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+
+      <MantineProvider
+        theme={{
+          fontFamily: "Cabinet Grotesk",
+          headings: {
+            fontFamily: "Cabinet Grotesk",
+            sizes: {
+              h1: { fontSize: 36 },
+            },
+          },
+        }}
+      >
+        <Notifications />
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 }
