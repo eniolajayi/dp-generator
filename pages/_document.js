9fe4827448ab01b49a0cc86d7c5b01912c6c4049
eniolajayi <ajayieniola16@gmail.com>
2023-03-26 04:18:35 +0100

    edit <head> content from index.html

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,21 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="description"
+          content="Create and Generate DP banners without the hassle for free"
+        />
+        <link rel="apple-touch-icon" href="/logo192.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="https://api.fontshare.com/css?f[]=cabinet-grotesk@300,400,500,700,800,900&display=swap"
+          rel="stylesheet"
+        />
+        <title>Auto DP Generator</title>
+      </Head>
+
       <body>
         <Main />
         <NextScript />
