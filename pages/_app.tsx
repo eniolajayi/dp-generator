1cf13460250aeb3f3532e58ada19a7cf88d74c69
eniolajayi <ajayieniola16@gmail.com>
2023-08-02 12:29:09 +0100

    refactor: prettify document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
-import '../styles/globals.css';
-import Head from 'next/head';
+import "../styles/globals.css";
+import Head from "next/head";
 import { MantineProvider } from "@mantine/core";
-import { Notifications } from '@mantine/notifications';
-import Layout from '../components/layout/Layout';
-import { AppProps } from 'next/app';
+import { Notifications } from "@mantine/notifications";
+import Layout from "../components/layout/Layout";
+import { AppProps } from "next/app";
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
@@ -12,17 +12,21 @@ export default function App(props: AppProps) {
     <>
       <Head>
         <title>DP Generator</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
       </Head>
 
       <MantineProvider
-      withGlobalStyles withNormalizeCSS
+        withGlobalStyles
+        withNormalizeCSS
         theme={{
           fontFamily: "Cabinet Grotesk",
           headings: {
             fontFamily: "Cabinet Grotesk",
             sizes: {
-              h1: { fontSize: '36' },
+              h1: { fontSize: "36" },
             },
           },
         }}
